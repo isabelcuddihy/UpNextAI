b497c2e0f122ab3695abbded1314ae1289c1f5e8
isabel-ann <104743014+isabel-ann@users.noreply.github.com>
2025-06-15 13:19:40 -0700

    Initial Commit

---
+++ NextUpAITests/NextUpAITests.swift
@@ -0,0 +1,17 @@
+//
+//  NextUpAITests.swift
+//  NextUpAITests
+//
+//  Created by Isabel Cuddihy on 6/15/25.
+//
+
+import Testing
+@testable import NextUpAI
+
+struct NextUpAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
